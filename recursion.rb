275df26b50393244edcf6e6d016f998801c228d8
Axton Smith <axtonsmith@gmail.com>
2022-07-22 11:49:07 -0400

    Finish problems 2 and 3

--- recursion.rb
+++ recursion.rb
@@ -1,7 +1,30 @@
 # frozen_string_literal: true
 
+require 'pry-byebug'
+
 def factorial(num)
   return 1 if num == 1
 
   num * factorial(num - 1)
+end
+
+def palindrome(str)
+  return true if str.length == 1 || str.length.zero?
+
+  return false unless str[0] == str[-1]
+
+  palindrome(str[1..-2])
+end
+
+def bottles(n)
+  if n.zero?
+    puts 'no more bottles of beer on the wall'
+  elsif n >= 2
+    puts "#{n} bottles of beer on the wall"
+    puts "#{n-1} bottles of beer on the wall"
+    bottles(n-2)
+  else
+    puts "#{n} bottles of beer on the wall"
+    puts 'no more bottles of beer on the wall'
+  end
 end
